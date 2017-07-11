e7c1024051bb9f90b823ece10bb10e259a151ae1
Tiago-Cruz <tiago_c5@hotmail.com>
2017-07-11 13:04:16 +0100

    Generate Getters and Setters

--- src/main/java/io/altar/parkee/model/ParkingSpot.java
+++ src/main/java/io/altar/parkee/model/ParkingSpot.java
@@ -6,4 +6,29 @@ public class ParkingSpot {
     private int number;
     private String row;
     private int statusId;
+    
+	public int getParkId() {
+		return parkId;
+	}
+	public void setParkId(int parkId) {
+		this.parkId = parkId;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public String getRow() {
+		return row;
+	}
+	public void setRow(String row) {
+		this.row = row;
+	}
+	public int getStatusId() {
+		return statusId;
+	}
+	public void setStatusId(int statusId) {
+		this.statusId = statusId;
+	}
 }
