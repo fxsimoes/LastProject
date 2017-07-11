e7f73e47501b08bd5ab31b1aabfeb8aa0f89a9df
tiagopontoxavier <tiagopontoxavier@gmail.com>
2017-07-11 10:46:23 +0100

    ParkStatus

---
+++ src/main/java/io/altar/parkee/model/ParkingStatus.java
@@ -0,0 +1,25 @@
+package io.altar.parkee.model;
+
+public class ParkingStatus {
+
+	private String Name;
+	private String Code;
+	
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getCode() {
+		return Code;
+	}
+	public void setCode(String code) {
+		Code = code;
+	}
+	
+	public ParkingStatus() {}
+
+}
+
+
