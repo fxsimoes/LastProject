3a3985dd72c90755aa02b60fbec3d8b1c14033e9
Alunomanha <alunomanha@Altars-Mini-108.lan>
2017-07-11 08:33:23 +0100

    park vars

--- src/main/java/io/altar/parkee/model/Park.java
+++ src/main/java/io/altar/parkee/model/Park.java
@@ -2,4 +2,48 @@ package io.altar.parkee.model;
 
 public class Park {
 
+	private int parkId;
+	private String parkName;
+	private int hourlyPrice;
+	private int nrOfSpots;
+	private int spotRef;
+	private String parkLocation;
+	public int getParkId() {
+		return parkId;
+	}
+	public void setParkId(int parkId) {
+		this.parkId = parkId;
+	}
+	public String getParkName() {
+		return parkName;
+	}
+	public void setParkName(String parkName) {
+		this.parkName = parkName;
+	}
+	public int getHourlyPrice() {
+		return hourlyPrice;
+	}
+	public void setHourlyPrice(int hourlyPrice) {
+		this.hourlyPrice = hourlyPrice;
+	}
+	public int getNrOfSpots() {
+		return nrOfSpots;
+	}
+	public void setNrOfSpots(int nrOfSpots) {
+		this.nrOfSpots = nrOfSpots;
+	}
+	public int getSpotRef() {
+		return spotRef;
+	}
+	public void setSpotRef(int spotRef) {
+		this.spotRef = spotRef;
+	}
+	public String getParkLocation() {
+		return parkLocation;
+	}
+	public void setParkLocation(String parkLocation) {
+		this.parkLocation = parkLocation;
+	}
+	
+	
 }
