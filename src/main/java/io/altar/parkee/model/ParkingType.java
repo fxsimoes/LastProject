5a7c374f820334b75b67bea2467c8a079323805f
Tiago-Cruz <tiago_c5@hotmail.com>
2017-07-11 10:28:49 +0100

    Commit ParkingType
    Genarate Getters and Setters

--- src/main/java/io/altar/parkee/model/ParkingType.java
+++ src/main/java/io/altar/parkee/model/ParkingType.java
@@ -5,4 +5,23 @@ public class ParkingType {
 	private int id;
 	private String nome;
 	private String code;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
 }
