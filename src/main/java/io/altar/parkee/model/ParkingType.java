fef36ddd5872407af80bbeeebc265ef77ab2b79e
tiagopontoxavier <tiagopontoxavier@gmail.com>
2017-07-11 10:19:15 +0100

    ParkingType_cenas

---
+++ src/main/java/io/altar/parkee/model/ParkingType.java
@@ -0,0 +1,28 @@
+package io.altar.parkee.model;
+
+public class ParkingType {
+
+	private int id;
+	private String nome;
+	private String code;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+}
--- src/main/java/io/altar/parkee/model/Parking_Type.java
+++
@@ -1,8 +0,0 @@
-package io.altar.parkee.model;
-
-public class Parking_Type {
-
-	private int id;
-	private String nome;
-	private String code;
-}
