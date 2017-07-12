b1991846a009b464731354112239ad2a7efdbdd3
tiagopontoxavier <tiagopontoxavier@gmail.com>
2017-07-12 01:40:36 +0100

    merdinhas em falta get/constr

--- src/main/java/io/altar/parkee/model/ParkingSpot.java
+++ src/main/java/io/altar/parkee/model/ParkingSpot.java
@@ -1,34 +1,45 @@
 package io.altar.parkee.model;
 
 public class ParkingSpot {
-	
-    private int parkId;
-    private int number;
-    private String row;
-    private int statusId;
-    
+
+	private int parkId;
+	private int number;
+	private String row;
+	private int statusId;
+
 	public int getParkId() {
 		return parkId;
 	}
+
 	public void setParkId(int parkId) {
 		this.parkId = parkId;
 	}
+
 	public int getNumber() {
 		return number;
 	}
+
 	public void setNumber(int number) {
 		this.number = number;
 	}
+
 	public String getRow() {
 		return row;
 	}
+
 	public void setRow(String row) {
 		this.row = row;
 	}
+
 	public int getStatusId() {
 		return statusId;
 	}
+
 	public void setStatusId(int statusId) {
 		this.statusId = statusId;
 	}
+
+	public ParkingSpot() {
+	}
+
 }
--- src/main/java/io/altar/parkee/model/ParkingStatus.java
+++ src/main/java/io/altar/parkee/model/ParkingStatus.java
@@ -4,22 +4,24 @@ public class ParkingStatus {
 
 	private String Name;
 	private String Code;
-	
+
 	public String getName() {
 		return Name;
 	}
+
 	public void setName(String name) {
 		Name = name;
 	}
+
 	public String getCode() {
 		return Code;
 	}
+
 	public void setCode(String code) {
 		Code = code;
 	}
-	
-	public ParkingStatus() {}
+
+	public ParkingStatus() {
+	}
 
 }
-
-
