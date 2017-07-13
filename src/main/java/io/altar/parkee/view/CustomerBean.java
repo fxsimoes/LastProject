562cc91603f5333730c271cc951071eaea0feb97
Tiago-Cruz <tiago_c5@hotmail.com>
2017-07-13 12:32:12 +0100

    Working in Customer class

--- src/main/java/io/altar/parkee/view/CustomerBean.java
+++ src/main/java/io/altar/parkee/view/CustomerBean.java
@@ -14,9 +14,70 @@ import io.altar.parkee.service.CustomerService;
 public class CustomerBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	private int customerId;
+	private String customerName;
+	private int customerContact;
+	private int nif;
 	
-	private Customer selectedCustomer = new Customer();
+	private Customer newCustomer = new Customer();
+	private Customer activeCustomer = new Customer();
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
 	
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public int getCustomerContact() {
+		return customerContact;
+	}
+
+	public void setCustomerContact(int customerContact) {
+		this.customerContact = customerContact;
+	}
+
+	public int getNif() {
+		return nif;
+	}
+
+	public void setNif(int nif) {
+		this.nif = nif;
+	}
+
+	public Customer getNewCustomer() {
+		return newCustomer;
+	}
+
+	public void setNewCustomer(Customer newCustomer) {
+		this.newCustomer = newCustomer;
+	}
+
+	public Customer getActiveCustomer() {
+		return activeCustomer;
+	}
+
+	public void setActiveCustomer(Customer activeCustomer) {
+		this.activeCustomer = activeCustomer;
+	}
+
 	@Inject
 	private CustomerService customerService;
+	
+	public CustomerService getCustomerService() {
+		return customerService;
+	}
+
+	public void setCustomerService(CustomerService customerService) {
+		this.customerService = customerService;
+	}
 }
