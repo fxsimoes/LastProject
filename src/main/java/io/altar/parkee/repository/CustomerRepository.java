764ce13c1aa9a1fdea5ba5bd5fecdec42d4a6627
Tiago-Cruz <tiago_c5@hotmail.com>
2017-07-17 11:57:32 +0100

    Working Customer

--- src/main/java/io/altar/parkee/repository/CustomerRepository.java
+++ src/main/java/io/altar/parkee/repository/CustomerRepository.java
@@ -11,7 +11,7 @@ import javax.transaction.Transactional;
 import io.altar.parkee.model.Customer;
 import io.altar.parkee.model.Park;
 
-@Named("customerRepository")
+@Named("CustomerRepository")
 @ApplicationScoped
 public class CustomerRepository extends EntityRepository<Customer> {
 	
@@ -19,35 +19,35 @@ public class CustomerRepository extends EntityRepository<Customer> {
 	
 	public List<Customer> getDbElements() {
 		
-		Query query = getDb().createQuery("FROM customer");
-		List<Customer> dbElements = (List<Customer>) query.getResultList();
-		return dbElements;
+		Query query = getEm().createQuery("FROM customer");
+		List<Customer> dbCustomers = (List<Customer>) query.getResultList();
+		return dbCustomers;
 	}
 	
 	@Override
 	@Transactional
 	public void removeFromDb(Customer customer){
 		
-		Customer customerToRemove = getDb().find(Customer.class, customer.getId());
-		getDb().remove(customerToRemove);
+		Customer customerToRemove = getEm().find(Customer.class, customer.getId());
+		getEm().remove(customerToRemove);
 	}
 	
-	@Transactional
-	public void modifyDb(int id, String customerName, int customerContact, int nif){
-		
-		Customer dbCustomer = getDb().find(Customer.class, id);
-		dbCustomer.setCustomerName(customerName);
-		dbCustomer.setCustomerContact(customerContact);		
-		dbCustomer.setNif(nif);
-	}
+//	@Transactional
+//	public void modifyDb(int id, String customerName, int customerContact, int nif){
+//		
+//		Customer dbCustomer = getEm().find(Customer.class, id);
+//		dbCustomer.setCustomerName(customerName);
+//		dbCustomer.setCustomerContact(customerContact);		
+//		dbCustomer.setNif(nif);
+//	}
 	
-	@Transactional
-	public Customer addToDb(int id, String customerName, int customerContact, int nif){
-		Customer newCustomer =  new Customer();
-		newCustomer.setCustomerName(customerName);
-		newCustomer.setCustomerContact(customerContact);	
-		newCustomer.setNif(nif);
-		em.persist(newCustomer);
-		return newCustomer;
-	}
+//	@Transactional
+//	public Customer addToDb(int id, String customerName, int customerContact, int nif){
+//		Customer newCustomer =  new Customer();
+//		newCustomer.setCustomerName(customerName);
+//		newCustomer.setCustomerContact(customerContact);	
+//		newCustomer.setNif(nif);
+//		em.persist(newCustomer);
+//		return newCustomer;
+//	}
 }
