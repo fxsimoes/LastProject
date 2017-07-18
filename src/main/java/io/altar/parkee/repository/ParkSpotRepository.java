590a586bb274604965415c5f90a3fd31d2654c66
tiagopontoxavier <tiagopontoxavier@gmail.com>
2017-07-18 11:39:51 +0100

    ParkSpottableonly

---
+++ src/main/java/io/altar/parkee/repository/ParkSpotRepository.java
@@ -0,0 +1,50 @@
+package io.altar.parkee.repository;
+
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+
+import io.altar.parkee.model.Customer;
+import io.altar.parkee.model.ParkSpot;
+
+@Named("ParkSpotRepository")
+@ApplicationScoped
+public class ParkSpotRepository extends EntityRepository<ParkSpot> {
+	
+	public List<ParkSpot> getDbElements() {
+		
+		Query query = getEm().createQuery("FROM ParkSpot");
+		List<ParkSpot> dbParkSpots = (List<ParkSpot>) query.getResultList();
+		return dbParkSpots;
+	}
+	
+	@Transactional
+	public void modifyDb(int id, int inputSpotNum, int number, String row, int statusId) {
+		ParkSpot dbParkSpot = getEm().find(ParkSpot.class, id);
+		dbParkSpot.setInputSpotNum(inputSpotNum);
+		dbParkSpot.setNumber(number);		
+		dbParkSpot.setRow(row);
+		dbParkSpot.setStatusId(statusId);	
+	}
+	
+	@Override
+	@Transactional
+	public void removeFromDb(ParkSpot parkSpot){
+		ParkSpot parkSpotToRemove = getEm().find(ParkSpot.class, parkSpot.getId());
+		getEm().remove(parkSpotToRemove);
+	}
+	
+//	@Transactional
+//	public ParkSpot addToDb(int id, int inputSpotNum, int number, String row, int statusId){
+//		ParkSpot newParkSpot =  new ParkSpot();
+//		newParkSpot.setInputSpotNum(inputSpotNum);
+//		newParkSpot.setNumber(number);	
+//		newParkSpot.setRow(row);
+//		newParkSpot.setStatusId(statusId);
+//		em.persist(newParkSpot);
+//		return newParkSpot;
+//	
+	}
---
+++ src/main/java/io/altar/parkee/view/ParkSpotBean.java
@@ -0,0 +1,64 @@
+package io.altar.parkee.view;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import io.altar.parkee.model.ParkSpot;
+import io.altar.parkee.service.ParkSpotService;
+
+@Named("ParkSpotBean")
+@RequestScoped
+public class ParkSpotBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private ParkSpot newParkSpot = new ParkSpot();
+	private ParkSpot activeParkSpot = new ParkSpot();
+	
+	public ParkSpot getNewParkSpot() {
+		return newParkSpot;
+	}
+
+	public void setNewParkSpot(ParkSpot newParkSpot) {
+		this.newParkSpot = newParkSpot;
+	}
+
+	public ParkSpot getActivePark() {
+		return activeParkSpot;
+	}
+
+	public void setActivePark(ParkSpot activeParkSpot) {
+		this.activeParkSpot = activeParkSpot;
+	}
+
+	public ParkSpotService getParkSpotService() {
+		return getParkSpotService();
+	}
+
+	public void setParkService(ParkSpotService parkSpotService) {
+		this.parkSpotService = parkSpotService;
+	}
+
+	@Inject
+	private ParkSpotService parkSpotService;
+	public Collection<ParkSpot> getSpots() {
+		return parkSpotService.showParks(parkSpotService.getParkRepository());
+	}
+	
+	public void addParkSpot(){
+		parkSpotService.addEntity(newParkSpot);
+	}
+
+//	public String deleteParkSpot(int id) {
+//		try {
+//			ParkSpotService.removeEntity(ParkSpotService.getParkSpotRepository(), activeParkSpot);
+//		} catch (NullPointerException e) {
+//			System.out.println("Exception Caught");
+//		}
+//		return null;
+//	}
+}
